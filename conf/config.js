fb21fb790b62f5405a08a835c8bca0badd1b71f8
mreboy <mreboy@gmail.com>
2026-01-20 01:16:00 -0500

    Update config.js
    Added commenting explaining some of the info that goes into this file.

--- conf/config.js
+++ conf/config.js
@@ -25,17 +25,26 @@ const logger = createLogger({
   ],
 });
 
+// NOTICE ABOUT fhghq URL variable: It MUST start with HTTPS !
+// If it starts with HTTP the server/client comms will break under any situation
+// where HTTPS is forcibly enforced either by the server or browser
+// because the WebSocket functions interpret this variable AS-IS and will
+// try to use HTTP if that is what is there !
+
 const config = env.getOrElseAll({
   logger,
   fhghq: {
-    url: 'http://hq.mreboy.com'
+    url: 'https://BASE_SITE_URL_HERE.com'
   },
+  // STEAM OpenID API Key required in order to use Steam for site login functions. See https://steamcommunity.com/dev/apikey to get one for free. REQUIRES FQDN for sign-up.
   steamApi: {
-    key: ''
+    key: 'API KEY STRING GOES HERE'
   },
+  //DEFAULT: Shard known as ABLE in-game. See https://github.com/clapfoot/warapi/ for full info on WarAPI & endpoints for BAKER or CHARLIE shards if needed
   warApi: {
     liveUrl: 'https://war-service-live.foxholeservices.com'
   },
+  // ALL DISCORD FUNCTIONS WERE COMMENTED OUT SOME TIME AGO - DON'T BOTHER PUTTING ANYTHING HERE
   discord: {
     token: ''
   },
