e118eb74d539bb1c705b6a73b1b019a7226960e7
Charles DuBose <kayakyakr@gmail.com>
2026-01-26 12:30:08 -0500

    Speaker audio-off icon.

--- src/components/global/page/Meta.js
+++ src/components/global/page/Meta.js
@@ -122,8 +122,7 @@ class SoundControl extends React.Component {
     //console.log("Updating sound control")
     let img = "/img/glitch/speaker_sm.png";
     if (!this.state.toggle) {
-      img =
-        "https://cdn.glitch.com/dd3f06b2-b7d4-4ccc-8675-05897efc4bb5%2F42.png?1559249871623";
+      img = "/img/glitch/speaker_sm_red.png";
     }
     return (
       <React.Fragment>
