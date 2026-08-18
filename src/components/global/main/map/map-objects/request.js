e5ed7d4a1af9cfcb69258032ab43bf5b1f7bbdfc
mreboy <mreboy@gmail.com>
2026-08-18 00:48:04 -0400

    Revert "Update request.js to catch errors"
    This reverts commit 954fc9c95e390f0712a34b8b967a2af8865cf4f8.

--- src/components/global/main/map/map-objects/request.js
+++ src/components/global/main/map/map-objects/request.js
@@ -1,99 +1,153 @@
-import React from 'react';
-import markers from '../../../../../_static/markers';
-const L = require('react-leaflet');
-import store from '../../../../../redux/store'
-import {connect} from 'react-redux';
-import A from '../../../../../redux/actions.js';
-import socket from '../../../../../_static/socket';
-import U from '../../../useful_functions'
+import React from "react";
+import markers from "../../../../../_static/markers";
+const L = require("react-leaflet");
+import store from "../../../../../redux/store";
+import { connect } from "react-redux";
+import A from "../../../../../redux/actions.js";
+import socket from "../../../../../_static/socket";
+import U from "../../../useful_functions";
+import { NoteAddon } from "./note-addon.js";
+import { AuthorAddon } from "./author-addon.js";
+
+const getRequest = (requests, position) => {
+  const signature = U.signature(position);
+  let request = requests[signature];
+  if (request == undefined) {
+    // the request may have been moved, we need to find it
+    request = Object.values(requests).find(
+      (r) => JSON.stringify(r.position) == JSON.stringify(position),
+    );
+  }
+  return request;
+};
 
 class RequestIcon_ extends React.Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
-      this.markerRef = React.createRef();
-    }
-      shouldComponentUpdate(nextProps,nextState){
-      let signature = U.signature(this.props.request.position)
-      //console.log("Request props",this.props,nextProps)
-        if(JSON.stringify(this.props.requests[signature])!=JSON.stringify(nextProps.requests[signature])){
-          return true
-        }
-          return false
-      }
-
-  SelectRequest(request){
-   let signature = U.signature(request.position)
-   store.dispatch(A.selectObject("requests",signature))
-  }
-  CheckCompleted(){ 
-    let signature = U.signature(this.props.request.position)
-    let request = this.props.requests[signature]
-    if(request.done==undefined){
-      return false
-    }
-    let donestring = JSON.stringify(request.done)
-    if(donestring==""){
-      return false
-    }
-    let done = JSON.parse(donestring)
-    for(var i=0;i<3;i++){
-      for(var j =0;j<request.request[i].length;j++){
-        let obj = request.request[i][j]
-        let item = done.find(item => obj.catid == item.catid&&obj.itemid == item.itemid);
-       // console.log("Request items",obj,item)
-        if(item==undefined){
-          return false;
-        }else{
-          if(obj.crates>item.crates){
-            return false;
-          }else{
-            item.crates = item.crates-obj.crates
-          }
-        }
-      }
-    }
-    return true;
-  }
-  handleDragEnd(e){
-    let obj = this.props.requests[U.signature(this.props.request.position)]
-    let coords = e.target._latlng
-    const position = {x:coords.lng,y:coords.lat}
-    let packet = {...obj, position: position, lastupdate:new Date()}
-    store.dispatch(A.updateObject("requests",packet, U.signature(position)))
-    store.dispatch(A.deleteObject("requests", U.signature(this.props.request.position)))
-    socket.emit('updateObject',{type:"requests",object:packet, key: U.signature(position)})
-    socket.emit('deleteObject',{type:"requests", key: U.signature(this.props.request.position)})
+    this.markerRef = React.createRef();
   }
 
-  render(){
-  //console.log("Rendering request")
-  let request = this.props.request
-  let icon = markers.RequestIcon.incomplete
-  if(this.CheckCompleted()){
-    icon=markers.RequestIcon.complete
-  }
-  try {
-    return (
-      <L.Marker ref={this.markerRef} 
-                position={[request.position.y,request.position.x]} 
-                icon={icon} 
-                onClick={()=>this.SelectRequest(this.props.request)}
-                draggable={true} 
-                onDragend={(e)=>this.handleDragEnd(e)}>
-        </L.Marker>
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      JSON.stringify(
+        getRequest(this.props.requests, this.props.request.position),
+      ) !=
+      JSON.stringify(
+        getRequest(nextProps.requests, this.props.request.position),
       )
-    }    
-  } catch (error) {
-    console.error(error)
-    return null
+    ) {
+      return true;
+    }
+    if (JSON.stringify(this.props.zoom) != JSON.stringify(nextProps.zoom)) {
+      return true;
+    }
+    return false;
+  }
+
+  get request() {
+    return getRequest(this.props.requests, this.props.request.position);
+  }
+
+  get signature() {
+    const request = this.request;
+    if (request && request.key) return request.key;
+    return U.signature(this.props.request.position);
+  }
+
+  SelectRequest() {
+    store.dispatch(A.selectObject("requests", this.signature));
+  }
+
+  CheckCompleted() {
+    const request = this.request;
+    if (!request.done || request.done.length === 0) {
+      return markers.RequestIcon.incomplete;
+    }
+
+    const combinedRequests = request.request.flat().reduce((acc, item) => {
+      const accItem = acc.find(
+        (accItem) =>
+          accItem.catid === item.catid && accItem.itemid === item.itemid,
+      );
+      if (accItem) {
+        accItem.crates += item.crates;
+        return acc;
+      }
+      return [
+        ...acc,
+        { catid: item.catid, itemid: item.itemid, crates: item.crates },
+      ];
+    }, []);
+    const totalLines = combinedRequests.reduce(
+      (acc, item) => item.crates + acc,
+      0,
+    );
+
+    request.done.forEach((item) => {
+      const requested = combinedRequests.find(
+        (r) => r.catid === item.catid && r.itemid === item.itemid,
+      );
+      requested.crates = Math.max(0, requested.crates - item.crates);
+    });
+    const completedLines =
+      totalLines - combinedRequests.reduce((acc, item) => item.crates + acc, 0);
+    const pct = completedLines / totalLines;
+
+    if (pct < 0.33) {
+      return markers.RequestIcon.incomplete;
+    }
+    if (pct < 0.66) {
+      return markers.RequestIcon.early;
+    }
+    if (pct < 1) {
+      return markers.RequestIcon.late;
+    }
+    return markers.RequestIcon.complete;
+  }
+
+  handleDragEnd(e) {
+    let coords = e.target._latlng;
+    const position = { x: coords.lng, y: coords.lat };
+    let packet = {
+      key: this.signature,
+      position: position,
+      lastupdate: new Date(),
+    };
+    store.dispatch(A.updateObject("requests", packet, this.signature));
+    socket.emit("updateObject", {
+      type: "requests",
+      object: packet,
+      key: this.signature,
+    });
+  }
+
+  render() {
+    //console.log("Rendering request")
+    let request = this.props.request;
+    let reqObj = getRequest(this.props.requests, request.position);
+    let icon = this.CheckCompleted();
+    return (
+      <React.Fragment>
+        {this.props.zoom > 3.5 && <AuthorAddon obj={reqObj} />}
+        {this.props.zoom > 3.5 && <NoteAddon obj={reqObj} />}
+        <L.Marker
+          ref={this.markerRef}
+          position={[request.position.y, request.position.x]}
+          icon={icon}
+          onClick={() => this.SelectRequest()}
+          draggable={true}
+          onDragend={(e) => this.handleDragEnd(e)}
+        ></L.Marker>
+      </React.Fragment>
+    );
   }
 }
 
-const mapStateToPropsRequests = store => {
-    let privateinfo = store.private
-    return {
-      requests: privateinfo.requests,
-    }
-  }
+const mapStateToPropsRequests = (store) => {
+  let privateinfo = store.private;
+  return {
+    requests: privateinfo.requests,
+  };
+};
 
 export const RequestIcon = connect(mapStateToPropsRequests)(RequestIcon_);
